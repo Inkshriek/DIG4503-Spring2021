b951686277297cc34f81ab4a67c37a78a2a4243c
Inkshriek <inkshriek@gmail.com>
2021-01-28 23:32:22 -0500

    Developed fetch request and finished lab 2

--- lab2/Fetch.js
+++ lab2/Fetch.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import chalk from 'chalk';
+
 class Fetch {
     constructor(pokemon, color) {
         this.pokemon = pokemon;
@@ -5,7 +8,13 @@ class Fetch {
     }
 
     fetch() {
-        //Will get to this
+        axios('https://pokeapi.co/api/v2/pokemon/' + this.pokemon)
+        .then( (response) => {
+            const pokemon = response.data;
+
+            console.log(chalk.hex(this.color)("This Pokémon is a " + pokemon.name + ", which has the ID number " + pokemon.id));
+        })
+        .catch( error => console.log(chalk.red("" + error)))
     }
 }
 
