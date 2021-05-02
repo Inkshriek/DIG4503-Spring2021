12c73a6f95fcc60f433ac4c316c98cc13e368f88
Inkshriek <inkshriek@gmail.com>
2021-05-01 22:11:37 -0400

    cleaning

--- portfolio2/client/src/components/Pokemon.js
+++ portfolio2/client/src/components/Pokemon.js
@@ -2,28 +2,21 @@ import axios from 'axios';
 import React from 'react';
 
 class Pokemon extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            pokemon: props.pokemon
-        };
-    }
-
     render() {
-        if (this.state.pokemon == null) {
-            return(
+        const pokemon = this.props.pokemon;
+        if (pokemon != null) {
+            return (
                 <div>
-                    <hr/>
-                    Slot is empty.
+                    <p>ID: {pokemon.pokeID}</p>
+                    <p>Name: {pokemon.name}</p>
+                    <img src={pokemon.img}></img>
                 </div>
             );
         }
         else {
-            return(
+            return (
                 <div>
-                    <p>ID: {this.state.pokemon.pokeID}</p>
-                    <p>Name: {this.state.pokemon.name}</p>
-                    <img src={this.state.pokemon.img}></img>
+                    <p>There's no Pokemon to display here.</p>
                 </div>
             );
         }
